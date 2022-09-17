8c071b18446c1381cdf7fb7217721fb6d969275a
victorbruce <victorbruce82@gmail.com>
2022-09-17 22:54:33 +0000

    add framer-motion

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-# NextJs Starter Project
+# NextJs Starter Kit
 
-Todo:
+A NextJs starter kit template for my personal needs.
+
+## Tech Stack
+
+- React (NextJs)
+- Typescript
+- TailwindCss
+- Framer Motion
+- Cypress
+- Github Actions
+
+## Folder Structure
+
+## State Management Setup
+
+### Todo:
+
 1. Husky and EsLint Rules.
+2. Add Google Analytics (**Done**)
+3. Add a theme Switcher (**Done**)
+4. Add e2e test using Cypress
+5. Add TailwindCss (**Done**)
+6. Base customization Typography, Font Sizes, Colors.
+7. Add CI/CD using Github Actions.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import Head from "next/head";
+import { motion } from "framer-motion";
+
+// components
 import ToggleThemeButton from "components/toggle-theme-button";
+
 const HomePage = () => {
   return (
     <div>
@@ -8,9 +12,14 @@ const HomePage = () => {
         <meta name="description" content="My next starter template project" />
       </Head>
       <main className="h-screen w-screen flex flex-col items-center justify-center bg-white dark:bg-black">
-        <h1 className="font-bold  text-3xl text-blue-500 dark:text-red-500 mb-4">
+        <motion.h1
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: [0.5, 0, 1], y: 0 }}
+          transition={{ duration: 1.2 }}
+          className="font-bold text-3xl text-blue-500 dark:text-red-500 mb-4"
+        >
           Hello World!
-        </h1>
+        </motion.h1>
         <ToggleThemeButton />
       </main>
     </div>
